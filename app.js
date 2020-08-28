b3ae55ebc71e84c98fc7ae58ac529e926c9807d9
Faraz <sayyedfaraz11@gmail.com>
2020-08-27 23:27:49 -0400

    origin master

--- app.js
+++ app.js
@@ -3,14 +3,13 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
-const modalBtn = document.querySelector('.modal-btn');
-const modalOverlay = document.querySelector('.modal-overlay');
-const closeBtn = document.querySelector('.close-btn');
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
 
-modalBtn.addEventListener('click', () => {
-    modalOverlay.classList.add('open-modal');
-})
-
-closeBtn.addEventListener('click', () => {
-    modalOverlay.classList.remove('open-modal');
-})
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
